8a1f54f551bc8bf41865ef586578f10a0f0f1f15
Nina Skryabina <alekseevnanina38@gmail.com>
2020-07-09 14:14:22 +0700

    Lesson_2 part_2

---
+++ lesson_2/2.py
@@ -0,0 +1,27 @@
+# friends = ['Mick', 'Wendy', 'Poll']
+#
+# print(friends)
+#
+# friends.append('Villo')
+#
+# print(friends)
+#
+# print(len(friends))
+#
+# del friends [1]
+#
+# print(friends)
+
+
+# hero = 'Iron Man'
+#
+# if hero.find('Iro') != -1:
+#     print('Есть')
+#
+# if 'ron' in hero:
+#     print('Yes')
+
+goals = ['Покормить кота', 'убраться', 'посмотреть фильм']
+
+if 'убраться' in goals:
+    print('Есть такая задача')
