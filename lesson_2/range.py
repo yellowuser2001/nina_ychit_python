4985af5f0f49bdc2f7e1f9fbd52b54e91b0bcafe
Nina Skryabina <alekseevnanina38@gmail.com>
2020-07-09 21:19:14 +0700

    range/

---
+++ lesson_2/range.py
@@ -0,0 +1,22 @@
+# numbers = range(10)
+#
+#  Вывести списком
+# print(list(numbers))
+#
+# winners = ['Konstantin', 'Oleg', 'Dmitriy']
+#
+# for i in range(len(winners)):
+#     print(i, ')', winners[i])
+
+numbers = [1, 3, 5]
+
+for number in numbers:
+    print(number)
+
+print(list(range(1, 100, 2)))
+
+for number in range(1, 100, 2):
+    print(number)
+
+
+
